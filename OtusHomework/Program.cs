ddcba5b17050efb2090a4c8adb29c45cddbea64f
Anton Chestikov <chestikov.anton@gmail.com>
2022-06-02 22:09:52 +0500

    Add calculation.

--- OtusHomework/Program.cs
+++ OtusHomework/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Calculate();
+        }
+
+        private static void Calculate()
+        {
+            const string StopWord = "стоп";
+            var allowedOperators = new char[] { '+', '-', '*', '/' };
+            const char Separator = ' ';
+
+            string userInput;
+
+            do
+            {
+                userInput = Console.ReadLine();
+
+                try
+                {
+                    var parts = userInput.Split(Separator);
+
+                    var firstOperand = int.Parse(parts[0]);
+                    var secondOperand = int.Parse(parts[2]);
+                    var operatorSign = parts[1][0];
+
+                    switch (operatorSign)
+                    {
+                        case '+':
+                            Sum(firstOperand, secondOperand);
+                            break;
+                        case '-':
+                            Sub(firstOperand, secondOperand);
+                            break;
+                        case '*':
+                            Mul(firstOperand, secondOperand);
+                            break;
+                        case '/':
+                            Div(firstOperand, secondOperand);
+                            break;
+                        default:
+                            break;
+                    }
+
+
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+
+            } while (!string.Equals(userInput, StopWord));
+        }
+
+        private static void Sum(int a, int b)
+        {
+            Console.WriteLine($"Ответ: {a + b}");
+        }
+
+        private static void Sub(int a, int b)
+        {
+            Console.WriteLine($"Ответ: {a - b}");
+        }
+
+        private static void Mul(int a, int b)
+        {
+            Console.WriteLine($"Ответ: {a * b}");
+        }
+
+        private static void Div(int a, int b)
+        {
+            Console.WriteLine($"Ответ: {a / b}");
+        }
+    }
+}
