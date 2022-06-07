992f64ddbf61c7a69a5478b2e65e4a2de199d13c
Anton Chestikov <chestikov.anton@gmail.com>
2022-06-07 22:55:35 +0500

    Add Stack class.

--- OtusHomework/Program.cs
+++ OtusHomework/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OtusHomework
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var s = new Stack("a", "b", "c");
+            // size = 3, Top = 'c'
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+            var deleted = s.Pop();
+            // Извлек верхний элемент 'c' Size = 2
+            Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
+            s.Add("d");
+            // size = 3, Top = 'd'
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+            s.Pop();
+            s.Pop();
+            s.Pop();
+            // size = 0, Top = null
+            Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
+
+            Console.ReadKey();
+
+            s.Pop();
+        }
+    }
+}
---
+++ OtusHomework/Stack.cs
@@ -0,0 +1,48 @@
+﻿namespace OtusHomework
+{
+    public class Stack
+    {
+        public int Size => _elements.Count();
+
+        public string Top
+        {
+            get
+            {
+                if (Size == 0)
+                {
+                    return null;
+                }
+
+                return _elements[_elements.Count - 1];
+            }
+        }
+
+        private List<string> _elements = new List<string>();
+
+        public Stack(params string[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                _elements.Add(elements[i]);
+            }
+        }
+
+        public void Add(string element)
+        {
+            _elements.Add(element);
+        }
+
+        public string Pop()
+        {
+            if (_elements.Count == 0)
+            {
+                throw new InvalidOperationException("Стек пустой");
+            }
+
+            var lastElement = Top;
+            _elements.RemoveAt(_elements.Count - 1);
+
+            return lastElement;
+        }
+    }
+}
