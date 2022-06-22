3251ae28d0abdb7c0bfb726b1921531e4ce3eb8f
Anton Chestikov <chestikov.anton@gmail.com>
2022-06-22 20:46:40 +0500

    Add Quadcopter.

---
+++ OtusHomework/Quadcopter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusHomework
+{
+    public class Quadcopter : IFlyingRobot, IChargeable
+    {
+        private List<string> _components = new() { "rotor1", "rotor2", "rotor3", "rotor4" };
+
+        public void Charge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetComponents() => _components;
+
+        public string GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
