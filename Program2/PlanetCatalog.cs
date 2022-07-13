7a401669d6236e52a2622a1deec26032d854b4d1
Anton Chestikov <chestikov.anton@gmail.com>
2022-07-13 23:53:16 +0500

    Add program2.

---
+++ Program2/PlanetCatalog.cs
@@ -0,0 +1,42 @@
+﻿namespace Program2
+{
+    public class PlanetCatalog
+    {
+        private List<Planet> _planets = new();
+        private int _allowedNumberOfRequests = 2;
+        private int _currentNumberOfRequests = 0;
+
+        public PlanetCatalog()
+        {
+            _planets.Add(new Planet("Венера", 38025, 2, _planets.LastOrDefault()));
+            _planets.Add(new Planet("Земля", 40076, 3, _planets.LastOrDefault()));
+            _planets.Add(new Planet("Марс", 21296, 4, _planets.LastOrDefault()));
+        }
+
+        public (int OrdinalNumberFromTheSun, int EquatorLength, string Error) GetPlanet(string name)
+        {
+            _currentNumberOfRequests++;
+
+            (int OrdinalNumberFromTheSun, int EquatorLength, string Error) output;
+
+            if (_currentNumberOfRequests > _allowedNumberOfRequests)
+            {
+                _currentNumberOfRequests = 0;
+                output = (0, 0, "Вы спрашиваете слишком часто");
+                return output;
+            }
+
+            var foundPlanet = _planets.Find(x => string.Equals(x.Name, name));
+            
+            if (foundPlanet is null)
+            {
+                output = (0, 0, "Не удалось найти планету");
+                return output;
+            }
+
+            output = (foundPlanet.OrdinalNumberFromTheSun, foundPlanet.EquatorLength, string.Empty);
+
+            return output;
+        }
+    }
+}
---
+++ Program2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Program2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var planets = new PlanetCatalog();
+
+            var planetsToSearch = new List<string>
+            {
+                "Земля", "Лимония", "Марс"
+            };
+
+            var sb = new StringBuilder();
+
+            foreach (var planet in planetsToSearch)
+            {
+                var foundPlanet = planets.GetPlanet(planet);
+
+                if (string.Equals(foundPlanet.Error, string.Empty))
+                {
+                    sb.AppendLine($"Название: {planet}, порядковый номер от Солнца: {foundPlanet.OrdinalNumberFromTheSun}, длина экватора: {foundPlanet.EquatorLength}\n");
+                }
+                else
+                {
+                    sb.AppendLine($"Планета {planet} не найдена. Ошибка: {foundPlanet.Error}\n");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
