6eff9250c14e0627b977554b0768185bdf801b9c
InvisibleGreenOrc <Chesantp@yandex.ru>
2022-08-29 22:32:47 +0500

    1-3 paragraph.

---
+++ OtusHomework/ImageDownloader.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace OtusHomework
+{
+    internal class ImageDownloader
+    {
+        public event Action DownloadStarted;
+
+        public event Action DownloadCompleted;
+
+        private readonly string _remoteUri;
+
+        private readonly string _fileName;
+
+        public ImageDownloader()
+        {
+            _remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            _fileName = "bigimage.jpg";
+        }
+
+        public void Download()
+        {
+            var myWebClient = new WebClient();
+
+            if (DownloadStarted is not null)
+            {
+                DownloadStarted();
+            }
+
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", _fileName, _remoteUri);
+            myWebClient.DownloadFile(_remoteUri, _fileName);
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", _fileName, _remoteUri);
+
+            if (DownloadCompleted is not null)
+            {
+                DownloadCompleted();
+            }
+        }
+    }
+}
--- OtusHomework/Program.cs
+++ OtusHomework/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main()
         {
+            var downloader = new ImageDownloader();
 
+            downloader.DownloadStarted += () => Console.WriteLine("Скачивание файла началось");
+            downloader.DownloadCompleted += () => Console.WriteLine("Скачивание файла закончилось");
+
+            downloader.Download();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
         }
     }
 }
