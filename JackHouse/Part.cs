2ca98e31f80b17469757ea332d25ad2444113252
InvisibleGreenOrc <Chesantp@yandex.ru>
2022-10-02 11:50:31 +0500

    Add JackHouse program.

---
+++ JackHouse/Part.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+
+namespace JackHouse
+{
+    internal class Part
+    {
+        public ImmutableList<string> Poem { get; private set; } = ImmutableList<string>.Empty;
+
+        private List<string> _stringsToAdd;
+
+        public Part(List<string> strings)
+        {
+            ArgumentNullException.ThrowIfNull(strings, nameof(strings));
+
+            _stringsToAdd = strings;
+        }
+
+        public void AddPart(ImmutableList<string> poem)
+        {
+            ArgumentNullException.ThrowIfNull(poem, nameof(poem));
+
+            Poem = poem.AddRange(_stringsToAdd);
+        }
+    }
+}
